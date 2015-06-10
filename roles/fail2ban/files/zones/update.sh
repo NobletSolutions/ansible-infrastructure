3c41bcaeca402911cb8c70a3d0077c503508fcee
Nathanael d. Noblet <nathanael@noblet.ca>
2015-06-10 09:05:28 -0600

    updated zone update method + added pakistan to block list

--- roles/fail2ban/files/zones/update.sh
+++ roles/fail2ban/files/zones/update.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+
 echo "create blacklist hash:net family inet hashsize 8192 maxelem 65536" > ../ipset.save;
 
-for zone in *zone; 
-do 
-  for ip in `cat $zone`; 
-  do 
-    echo "add blacklist $ip" >> ../ipset.save; 
-  done; 
+for country in bh br cn hk id in iq ir pk it ru sa th;
+do
+  wget http://www.ipdeny.com/ipblocks/data/aggregated/${country}-aggregated.zone
+  for ip in `cat ${country}-aggregated.zone`;
+  do
+     echo "add blacklist $ip" >> ../ipset.save;
+  done;
+  rm -f ${country}-aggregated.zone
 done
+
