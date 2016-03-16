064b901238d7f2ccb80196f396fed2d2de5f415b
Nathanael d. Noblet <nathanael@noblet.ca>
2016-03-16 13:08:04 -0600

    block taiwan

--- roles/fail2ban/files/update-zones.sh
+++ roles/fail2ban/files/update-zones.sh
@@ -2,7 +2,7 @@
 
 echo "create blacklist hash:net family inet hashsize 8192 maxelem 65536" > ../ipset.save;
 
-for country in bh br cn hk id in iq ir md mx pk it ru sa th tn;
+for country in bh br cn hk id in iq ir md mx pk it ru sa th tn tw;
 do
   wget http://www.ipdeny.com/ipblocks/data/aggregated/${country}-aggregated.zone
   for ip in `cat ${country}-aggregated.zone`;
