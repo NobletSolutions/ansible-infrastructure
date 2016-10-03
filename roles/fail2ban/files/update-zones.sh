d86250d9891529e33e324f168804310bd0990372
Nathanael d. Noblet <nathanael@noblet.ca>
2016-10-03 11:06:09 -0600

    updated banned countries

--- roles/fail2ban/files/update-zones.sh
+++ roles/fail2ban/files/update-zones.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo "create blacklist hash:net family inet hashsize 8192 maxelem 65536" > ../ipset.save;
+echo "create blacklist hash:net family inet hashsize 8192 maxelem 65536" > ipset.save;
 
-for country in bh br cn hk id in iq ir md mx pk it ru sa th tn tw;
+for country in bh br bz cn hk id in iq ir md mx pk it ru sa th tn tw;
 do
   wget http://www.ipdeny.com/ipblocks/data/aggregated/${country}-aggregated.zone
   for ip in `cat ${country}-aggregated.zone`;
   do
-     echo "add blacklist $ip" >> ./ipset.save;
+     echo "add blacklist $ip" >> ipset.save;
   done;
   rm -f ${country}-aggregated.zone
 done
