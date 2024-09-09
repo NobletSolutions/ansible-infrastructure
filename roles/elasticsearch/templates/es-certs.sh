8b304a860d5755e05a8444f0a2bc743a9c016797
Mark Woynarski <143827+mwoynarski@users.noreply.github.com>
2024-09-09 15:50:31 -0600

    Added deployment config for elasticsearch

---
+++ roles/elasticsearch/templates/es-certs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp /etc/letsencrypt/live/{{ elasticsearch_hostname }}/* /etc/elasticsearch/certs/
+chown root:elasticsearch /etc/elasticsearch/certs/*
+chcon -u system_u -t etc_t /etc/elasticsearch/certs/*
+chmod 640 /etc/elasticsearch/certs/*
